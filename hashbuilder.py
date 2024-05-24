c03bb18961fbce6f4a8055992ba9dbe098d40876
Mekelachi001 <mekelachichijioke@gmail.com>
2024-05-24 23:00:38 +0100

    Add files via upload

---
+++ hashbuilder.py
@@ -0,0 +1,28 @@
+# python password-hash cracking tool
+
+# Coded by Chijioke Mekelachi
+
+# Import needed modules
+
+# Hashlib module for building password hashes
+
+import hashlib
+
+# Import os modules to interact with the os terminal
+
+import os
+
+# Import time module 
+
+import time
+
+# Import made python file
+
+from fontloading import main
+
+# Build loading front
+
+main()
+# Resquset first input 
+# Build or Crack password
+print("\n\n[1] Build Password Hash\n[2]Crack password hash")
